f127ef88f2333451da9635c1bd55b9298cce90fe
chzhao <chzhao@wisdombud.com>
2018-01-11 09:41:33 +0800

    init

---
+++ zFileDb/BaseBianryBll.cs
@@ -0,0 +1,18 @@
+﻿
+namespace wardensky.xmldb
+{
+    public class BaseBinaryBll<T> : AbstractFileDbBll<T> where T : new()
+    {
+        public BaseBinaryBll()
+        {
+            this.bll = BinarySerializerBll<T>.GetInstance();
+        }
+
+        public override string DbFile
+        {
+            get { return this.bll.Dbfile; }
+            set { bll.Dbfile = value; }
+        }
+
+    }
+}
